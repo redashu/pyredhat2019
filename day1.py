e896687d9ae4401649068a82c742b5282976459f
redashu <ashutoshh@linux.com>
2019-01-29 20:11:46 +0530

    day1 completd

---
+++ day1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import  time
+x=10
+y=30
+
+#  swapping numbers
+x,y=y,x
+
+print  x
+time.sleep(2)
+print  y
+
+#  creating  tuple 
+t=[4,6,'hii',7]
+#  string  creation 
+s="hello world"
+time.sleep(3)
+print   s+" "+t[2]
+
---
+++ sample.py
@@ -0,0 +1,18 @@
+import  time
+x=10
+y=30
+
+#  swapping numbers
+x,y=y,x
+
+print  x
+time.sleep(2)
+print  y
+
+#  creating  tuple 
+t=[4,6,'hii',7]
+#  string  creation 
+s="hello world"
+time.sleep(3)
+print   s+" "+t[2]
+
